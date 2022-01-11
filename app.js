b0aae6081d750086c50523a797883c9af51a5b5c
httpDerpyy <gabriel.destro56@gmail.com>
2022-01-11 11:27:45 -0300

    registro de cep

--- app.js
+++ app.js
@@ -3,76 +3,249 @@
 const express = require('express')
 const path = require('path')
 const fetch = require('node-fetch')
-const res = require('express/lib/response')
+const cors = require('cors')
 
 const app = express()
 const port = 25565
 
 const cepFlashback = "-23.4594742,-46.5365758"
 
-app.get("/", ( request, response ) => {
-    response.sendFile( path.join(__dirname, 'index.html') )
+app.use(cors())
+app.use(express.static(__dirname + '/public'))
+
+// Banco de Dados
+const { MongoClient } = require('mongodb');
+const url = "mongodb+srv://Gabriel:shadowcat24@clusterflashback.3putl.mongodb.net/test"
+
+// End-points
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
-app.get("/cep", ( request, res ) => {
+app.get("/config", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/configuração.html"))
+})
 
-    const cepAlvo = request.query.alvo
+app.get("/registro", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/registro.html"))
+})
+
+app.get("/cep", (req, res) => {
+
+    const cepAlvo = req.query.alvo
     let responseData = {}
 
-    // Puxar Geolocalização
-    const geo = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAcUTU8IULsndQzoydzuzQkdMJFBIBzyg8&address=" + cepAlvo
-    
-    fetch(geo)
-    .then( response => response.json() )
-    .then( data => {
+    // Tentar buscar CEP no banco de dados privado primero
+    MongoClient.connect(url, (err, database) => {
 
-        if ( data.status != "OK" ) {
-            console.log( data )
-            res.send( { status: "ERROR" } )
+        if (err) { console.log(err); res.send({ status: "ERROR!" }); return }
+
+        let banco_geral = database.db('cepsoftware')
+        let ceps = banco_geral.collection('cepsregistrados')
+
+        try {
+            ceps.findOne({ cep: Number(cepAlvo) })
+                .then(data => {
+                    if (data != null) { // Temos esse CEP registrado!
+
+                        console.log(data)
+                        data.status = "OK"
+                        res.send(data)
+                        return;
+
+                        
+                    } else { // Usar a API do Google
+
+                        // Puxar Geolocalização
+                        const geo = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAcUTU8IULsndQzoydzuzQkdMJFBIBzyg8&address=" + cepAlvo
+
+                        fetch(geo)
+                            .then(response => response.json())
+                            .then(data => {
+
+                                if (data.status != "OK") {
+                                    console.log(data)
+                                    res.send({ status: "ERROR" })
+                                    return;
+                                }
+
+                                // Puxar Endereço
+                                const lat = data.results[0].geometry.location.lat
+                                const lng = data.results[0].geometry.location.lng
+
+                                const geo2 = `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAcUTU8IULsndQzoydzuzQkdMJFBIBzyg8&latlng=${lat},${lng}`
+
+                                fetch(geo2)
+                                    .then(response => response.json())
+                                    .then(data => {
+
+                                        let components = data.results[0].address_components
+
+                                        if (components == undefined) {
+                                            res.send({ status: "ERROR!" })
+                                            return;
+                                        }
+
+                                        responseData.address = components[1].short_name + ", " + components[2].short_name + ", " + components[6].short_name
+
+                                        // Checagem de distancia
+                                        const check = `https://maps.googleapis.com/maps/api/directions/json?origin=${cepFlashback}&destination=${cepAlvo}&key=AIzaSyAcUTU8IULsndQzoydzuzQkdMJFBIBzyg8`
+
+                                        fetch(check)
+                                            .then(response => response.json())
+                                            .then(data => {
+
+                                                if (data.routes[0] == undefined) {
+                                                    res.send({ status: "ERROR" })
+                                                    return;
+                                                }
+
+                                                responseData.distanceText = data.routes[0].legs[0].distance.text
+                                                responseData.distanceValue = data.routes[0].legs[0].distance.value
+                                                responseData.status = "OK"
+
+                                                res.send(responseData)
+
+                                            })
+
+                                    })
+
+                            })
+
+                    }
+                })
+
+        } catch (e) {
+            console.log(e)
+            res.send({status: "ERROR"})
             return;
         }
 
-        // Puxar Endereço
-        const lat = data.results[0].geometry.location.lat
-        const lng = data.results[0].geometry.location.lng
+    })
+})
 
-        const geo2 = `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAcUTU8IULsndQzoydzuzQkdMJFBIBzyg8&latlng=${lat},${lng}`
+app.get("/data", (req, res) => {
 
-        fetch(geo2)
-        .then( response => response.json() )
-        .then( data => {
+    let responseBody = {}
 
-            let components = data.results[0].address_components
-            responseData.address = components[1].short_name + ", " + components[2].short_name + ", " + components[3].short_name + ", " + components[6].short_name 
+    // Conecta-se ao banco de dados
+    MongoClient.connect(url, (err, database) => {
 
-            // Checagem de distancia
-            const check = `https://maps.googleapis.com/maps/api/directions/json?origin=${cepFlashback}&destination=${cepAlvo}&key=AIzaSyAcUTU8IULsndQzoydzuzQkdMJFBIBzyg8`
+        if (err) { console.log(err); res.send({ status: "ERROR!" }); return }
 
-            fetch(check)
-            .then( response => response.json() )
-            .then( data => {
-        
-                if ( data.routes[0] == undefined ) {
-                    res.send({status: "ERROR"})
-                    return;
-                }
+        // Banco de Dados
+        let banco_geral = database.db('cepsoftware')
 
-                responseData.distanceText = data.routes[0].legs[0].distance.text
-                responseData.distanceValue = data.routes[0].legs[0].distance.value
-                responseData.status = "OK"
+        // Motoboys
+        let banco_motoboys = banco_geral.collection('motoboys')
+        try {
+            banco_motoboys.find({}).toArray()
+                .then(formatted => {
 
-                res.send( responseData )
-        
-            } )
+                    responseBody.Motoboys = formatted
 
-        })
+                    // Metragens
+                    let banco_metragens = banco_geral.collection('kilometragem')
+                    try {
+                        banco_metragens.find({}).toArray()
+                            .then(formatted_kilometragem => {
 
-    } )
+                                console.log(formatted_kilometragem)
 
+                                responseBody.Metragens = formatted_kilometragem
 
+                                responseBody.status = "OK"
+                                res.send(responseBody)
+                                return;
+
+                            })
+                    } catch (e) {
+                        console.log(e)
+                        res.send({ status: "ERROR!" })
+                        return;
+                    }
+
+                })
+
+        } catch (e) {
+            console.log(e)
+            res.send({ status: "ERROR!" })
+            return;
+        }
+
+    })
 
 })
 
+// HTTP Posts
+app.post("/data", (req, res) => {
+    req.on('data', (data) => {
+        data = JSON.parse(data)
+        // Conecta-se ao banco de dados
+        MongoClient.connect(url, (err, database) => {
+
+            if (err) { console.log(err); res.send({ status: "ERROR!" }); return }
+
+            // Banco de Dados
+            let banco_geral = database.db('cepsoftware')
+
+            // Motoboys
+            let banco_motoboys = banco_geral.collection('motoboys')
+            try {
+                banco_motoboys.deleteMany({})
+                banco_motoboys.insertMany(data.Motoboys)
+            } catch (e) {
+                console.log(e)
+                res.send({ status: "ERROR!" })
+                return;
+            }
+
+            // Metragens
+            let banco_metragens = banco_geral.collection('kilometragem')
+            try {
+                banco_metragens.deleteMany({})
+                banco_metragens.insertMany(data.Metragens)
+            } catch (e) {
+                console.log(e)
+                res.send({ status: "ERROR!" })
+                return;
+            }
+
+            res.send({ status: "OK" })
+
+        })
+
+    })
+
+})
+
+app.post("/registroCEP", (req, res) => {
+    req.on('data', (data) => {
+
+        data = JSON.parse(data)
+        console.log(data)
+        // Conecta-se ao banco de dados
+        MongoClient.connect(url, (err, database) => {
+
+            if (err) { console.log(err); res.send({ status: "ERROR!" }); return }
+
+            let banco_geral = database.db('cepsoftware')
+            let registro_ceps = banco_geral.collection('cepsregistrados')
+
+            try {
+                registro_ceps.insertOne(data.Package)
+            } catch (e) {
+                res.send({ status: "ERROR" })
+                console.log(e)
+                return;
+            }
+
+            res.send({ status: "OK" })
+            return;
+
+        })
+    })
+})
 
 app.listen(port, () => {
     console.log('Ready!')
